fac7c676bb4b6e6d151bf9a76bdd3f2f926d3c67
AliRodja <rodjaali@gmail.com>
2024-11-03 17:19:25 +0700

    Belajar Laravel 11 | 5. Blade Component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Blog</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman Blog</h1>
-    <article>
-        <h3>Judul 1</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque sunt perspiciatis eveniet ea maiores perferendis animi modi obcaecati magnam corrupti aliquid veritatis iste sed, aliquam repellat, magni libero voluptatibus distinctio.</p>
-    </article>
-
-    <article>
-        <h3>Judul 2</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque sunt perspiciatis eveniet ea maiores perferendis animi modi obcaecati magnam corrupti aliquid veritatis iste sed, aliquam repellat, magni libero voluptatibus distinctio.</p>
-    </article>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcome to my Blog</h3>
+</x-layout>
