76582d316881bd9cc312bd605ce8dc2b08896345
AliRodja <rodjaali@gmail.com>
2024-12-30 09:37:39 +0700

    Belajar Laravel 11 | 12. Post Category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,12 @@ class Post extends Model
    {
       return $this->belongsTo(User::class);
    }
+   
+   public function category(): BelongsTo
+   {
+      return $this->belongsTo(Category::class);
+      
+   }
 }
 
 ?>
