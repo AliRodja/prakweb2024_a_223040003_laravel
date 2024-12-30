a87c648b1843006abf8360acd81f56d446e98e4e
AliRodja <rodjaali@gmail.com>
2024-12-30 12:59:21 +0700

    Belajar Laravel 11 | 14. N + 1 Problem

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,8 @@ class Post extends Model
    use HasFactory;
    protected $fillable = ['title', 'author', 'slug', 'body'];
 
+   protected $with = ['category', 'author'];
+
    public function author():BelongsTo
    {
       return $this->belongsTo(User::class);
