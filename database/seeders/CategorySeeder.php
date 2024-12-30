2464061615294a9585033f1322d961b543244371
AliRodja <rodjaali@gmail.com>
2024-12-30 10:57:08 +0700

    Belajar Laravel 11 | 13. Database Seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design',
+        ]);
+
+        Category::create([
+            'name' => 'Ui/Ux Design',
+            'slug' => 'ui-ux',
+        ]);
+
+        Category::create([
+            'name' => 'Machine Learning',
+            'slug' => 'machine-learning',
+        ]);
+
+        Category::create([
+            'name' => 'Data Analysis',
+            'slug' => 'data-analysis',
+        ]);
+
+    }
+}
